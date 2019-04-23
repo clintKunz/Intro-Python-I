14f06b0efb01e54849d854b2c4b8c3126930be9c
Clint Kunz <clintkunz@gmail.com>
2019-04-23 16:15:40 -0600

    args file complete

--- src/args.py
+++ src/args.py
@@ -5,13 +5,28 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x, y):
+    return x + y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    total = 0
+    for arg in args:
+        if type(arg) == type(0):
+            total += arg
+        elif type(arg) == type([]):
+            for i in arg:
+                if type(i) == type(0):
+                    total += i
+                else:
+                    print("type not recognized")
+        else:
+            print("type not recognized")
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -28,6 +43,8 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y=1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +59,12 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*positional, **keywords):
+    for i in positional:
+        for key in i:
+            print("key: ", key + ', value: ', i[key])
+    for i in keywords:
+        print("key: ", i + ', value: ', keywords[i])
 
 # Should print
 # key: a, value: 12
